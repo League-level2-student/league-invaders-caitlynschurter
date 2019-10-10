38003e1e0fa98685258d2afcac0368083b89a8eb
SMHS-519-08 <SMHS-519-08@SMHS-519-08.smhsrop.lab>
2019-10-10 16:25:27 -0700

    League Invaders P1   10/10/19

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,22 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	final int width = 500;
+	final int height = 800;
 
+	public static void main(String[] args) {
+new LeagueInvaders().setup();
+	}
+
+	LeagueInvaders() {
+		frame = new JFrame();
+	}
+
+	void setup() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(width, height);
+		frame.pack();
+	}
 }
