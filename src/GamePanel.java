010fe426f7f5b04c702e5b576c09458a9bb2fc17
SMHS-519-08 <SMHS-519-08@SMHS-519-08.smhsrop.lab>
2019-10-24 15:29:35 -0700

    LeagueInvaders Part 6   10/24/19

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 
 //Evil Lily's password: JesusChrist123!
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,31 +11,94 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
-	GameObject gameObject;
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
 
 	public GamePanel() {
 		timer = new Timer(1000 / 60, this);
-		gameObject = new GameObject(20, 30, 40, 50);
 	}
 
 	void startGame() {
 		timer.start();
 	}
 
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.width, LeagueInvaders.height);
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
-		gameObject.draw(g);
+		switch (currentState) {
+		case MENU_STATE:
+			drawMenuState(g);
+			break;
+		case GAME_STATE:
+			drawGameState(g);
+			break;
+		case END_STATE:
+			drawEndState(g);
+			break;
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		gameObject.update();
 		repaint();
+		/*
+		 * if (currentState == MENU_STATE) { updateMenuState(); }
+		 * 
+		 * else if (currentState == GAME_STATE) { updateGameState(); }
+		 * 
+		 * else if (currentState == END_STATE) { updateEndState(); }
+		 */
+
+		switch (currentState) {
+		case MENU_STATE:
+			updateMenuState();
+			break;
+		case GAME_STATE:
+			updateGameState();
+			break;
+		case END_STATE:
+			updateEndState();
+			break;
+		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		System.out.println("hi");
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			currentState++;
+			if (currentState > END_STATE) {
+				currentState = MENU_STATE;
+			}
+		}
 
 	}
 
